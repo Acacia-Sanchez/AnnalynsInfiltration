63077e35ad2292ffacff9e357933fc484ed0901b
Acacia-Sanchez-Pastur <sanchezpastur@gmail.com>
2024-11-04 18:32:24 +0100

    methods made and tests coverage 95%

--- src/main/java/org/factoriaf5/infiltration/AnnalynsInfiltration.java
+++ src/main/java/org/factoriaf5/infiltration/AnnalynsInfiltration.java
@@ -3,20 +3,33 @@ package org.factoriaf5.infiltration;
 public class AnnalynsInfiltration {
 
     public static boolean canFastAttack(boolean knightIsAwake) {
-        // Implement method
-    }
+        if (knightIsAwake) {
+            return false;
+        }
+        return true;
+    } // el return indica si se puede realizar o no el fast attack,
+      // si está dormido retorna true al método, que significa que se puede hacer fast
+      // attack (ME HA COSTADO ENTENDERLO JEJEJEJE)
 
     public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
-        // Implement method
+        if (knightIsAwake || archerIsAwake || prisonerIsAwake) {
+            return true;
+        }
+        return false;
     }
 
     public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
-        // Implement method
+        if (prisonerIsAwake && !archerIsAwake) {
+            return true;
+        }
+        return false;
     }
 
     public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake,
             boolean petDogIsPresent) {
-        // Implement method
+        if ((!archerIsAwake && petDogIsPresent) || (prisonerIsAwake && !knightIsAwake && !archerIsAwake)) {
+            return true;
+        }
+        return false;
     }
-
 }
